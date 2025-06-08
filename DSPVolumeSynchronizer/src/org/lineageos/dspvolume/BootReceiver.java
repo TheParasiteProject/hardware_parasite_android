8b1248cee8f20276d226614e1c3808f30fe4fd66
someone5678 <59456192+someone5678@users.noreply.github.com>
2025-06-08 23:35:27 +0900

    DSPVolumeSynchronizer: Tidy up
    * Run google-java-format
    * Use constants
    
    Change-Id: I85e4f58ddcb23c6108ef78caa0a9df523e9626fb

--- DSPVolumeSynchronizer/src/org/lineageos/dspvolume/BootReceiver.java
+++ DSPVolumeSynchronizer/src/org/lineageos/dspvolume/BootReceiver.java
@@ -4,7 +4,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
-
 public class BootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(final Context context, Intent intent) {
