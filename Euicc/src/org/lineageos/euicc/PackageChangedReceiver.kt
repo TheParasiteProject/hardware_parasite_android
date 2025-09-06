LineageOS/android_hardware_sony

Euicc: Add package change listener
This lets us toggle SIM Manager automatically whenever user installs
or uninstalls microG.

Change-Id: I4fe077f0dab7bf121883831c14e773fba09e6c5b

Euicc/src/org/lineageos/euicc/PackageChangedReceiver.kt
@@ -1,0 +1,22 @@
+/*
+ * Copyright (C) 2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.euicc
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+
+class PackageChangedReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        Log.d(TAG, "Received package changed intent")
+        EuiccDisabler.enableOrDisableEuicc(context)
+    }
+
+    companion object {
+        private const val TAG = "SonyEuiccPackageReceiver"
+    }
+}

TheParasiteProject/hardware_parasite_android

Euicc/src/org/lineageos/euicc/PackageChangedReceiver.kt
@@ -1,0 +1,22 @@
+/*
+ * Copyright (C) 2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.euicc
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+
+class PackageChangedReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        Log.d(TAG, "Received package changed intent")
+        EuiccDisabler.enableOrDisableEuicc(context)
+    }
+
+    companion object {
+        private const val TAG = "LineageEuiccPackageReceiver"
+    }
+}