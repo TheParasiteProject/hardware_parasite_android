nekoshirro/android_hardware_dolby

dolby: Introduce Dolby Atmos
Moved from marble/sm8450-common

History:

commit 82fe03168c0402e4cb10d25859c3b398c0ef654a
Author: Adithya R <gh0strider.2k18.reborn@gmail.com>
Date:   Thu Mar 21 21:35:36 2024 +0530

    marble: parts: Restore dolby profile on audio changes

    Something keeps resetting back at random times, from what I observed,
    after resuming media or on a device change, lets workaround that.

    Change-Id: Id065f2482636194655c2399f0c35ad56b8e7a29d

commit c4400bd1326f65aeac1d0f26bb830ce7fd079773
Author: Adithya R <gh0strider.2k18.reborn@gmail.com>
Date:   Fri Feb 2 09:29:08 2024 +0530

    marble: parts/keyhandler: Guard debug logging

    Change-Id: I246941f26cd1f71b696eb3c996794c9baa5dbc00

commit f11b70a98a11d0b89673d73002996aed9f11fbd7
Author: Adithya R <gh0strider.2k18.reborn@gmail.com>
Date:   Sun Dec 31 20:36:52 2023 +0530

    marble: parts: Re-enable speaker virtualization after bootup

    For whatever reason, speaker virtualization isn't automatically
    restored at bootup unlike the other parameters. It was reported to be
    fixed by connecting and disconnecting headphones or disabling and
    enabling the toggle, so let's just automate that at bootup.

commit abcff4fb947c89b69c1d25bd290fd91b7873af6a
Author: Adithya R <gh0strider.2k18.reborn@gmail.com>
Date:   Fri Oct 20 06:49:19 2023 +0530

    marble: parts: Implement profile-specific Dolby settings

    Some refactoring and cleanup while we're at it.

commit dc54f9ddeff212d017b0cba16e56516e99335bb3
Author: Adithya R <gh0strider.2k18.reborn@gmail.com>
Date:   Mon Oct 9 21:58:58 2023 +0530

    marble: parts: Remove play/pause hack while toggling Dolby

    Not required with/fixed by:
    35217: audioflinger: Do not allow DAP effect to be suspended | https://gerrit.aospa.co/c/AOSPA/android_frameworks_av/+/35217

commit dd2acc8e0c10d05f86ff229412cc9f72ea242b44
Author: Adithya R <gh0strider.2k18.reborn@gmail.com>
Date:   Wed Sep 13 21:41:20 2023 +0530

    marble: parts: Set proper

dolby/src/co/aospa/dolby/xiaomi/BootCompletedReceiver.java
@@ -1,0 +1,29 @@
+/*
+ * Copyright (C) 2023-24 Paranoid Android
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package co.aospa.dolby.xiaomi;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class BootCompletedReceiver extends BroadcastReceiver {
+
+    private static final String TAG = "XiaomiParts-BCR";
+
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        Log.d(TAG, "Received intent: " + intent.getAction());
+        if (!intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+            return;
+        }
+
+        Log.i(TAG, "Boot completed, starting dolby");
+        DolbyUtils.getInstance(context).onBootCompleted();
+    }
+
+}

dolby/src/co/aospa/dolby/xiaomi/DolbyUtils.java
@@ -1,0 +1,281 @@
+/*
+ * Copyright (C) 2023-24 Paranoid Android
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package co.aospa.dolby.xiaomi;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.media.AudioAttributes;
+import android.media.AudioDeviceAttributes;
+import android.media.AudioDeviceCallback;
+import android.media.AudioDeviceInfo;
+import android.media.AudioManager;
+import android.media.AudioManager.AudioPlaybackCallback;
+import android.media.AudioPlaybackConfiguration;
+import android.media.session.MediaSessionManager;
+import android.os.Handler;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import co.aospa.dolby.xiaomi.DolbyConstants.DsParam;
+import co.aospa.dolby.xiaomi.R;
+
+import java.util.Arrays;
+import java.util.List;
+
+public final class DolbyUtils {
+
+    private static final String TAG = "DolbyUtils";
+    private static final int EFFECT_PRIORITY = 100;
+    private static final int VOLUME_LEVELER_AMOUNT = 2;
+
+    private static final AudioAttributes ATTRIBUTES_MEDIA = new AudioAttributes.Builder()
+            .setUsage(AudioAttributes.USAGE_MEDIA)
+            .build();
+
+    private static DolbyUtils mInstance;
+    private DolbyAtmos mDolbyAtmos;
+    private Context mContext;
+    private AudioManager mAudioManager;
+    private Handler mHandler = new Handler();
+    private boolean mCallbacksRegistered = false;
+
+    // Restore current profile on every media session
+    private final AudioPlaybackCallback mPlaybackCallback = new AudioPlaybackCallback() {
+        @Override
+        public void onPlaybackConfigChanged(List<AudioPlaybackConfiguration> configs) {
+            boolean isPlaying = configs.stream().anyMatch(
+                    c -> c.getPlayerState() == AudioPlaybackConfiguration.PLAYER_STATE_STARTED);
+            dlog("onPlaybackConfigChanged isPlaying=" + isPlaying);
+            if (isPlaying)
+                setCurrentProfile();
+        }
+    };
+
+    // Restore current profile on audio device change
+    private final AudioDeviceCallback mAudioDeviceCallback = new AudioDeviceCallback() {
+        public void onAudioDevicesAdded(AudioDeviceInfo[] addedDevices) {
+            dlog("onAudioDevicesAdded");
+            setCurrentProfile();
+        }
+
+        public void onAudioDevicesRemoved(AudioDeviceInfo[] removedDevices) {
+            dlog("onAudioDevicesRemoved");
+            setCurrentProfile();
+        }
+    };
+
+    private DolbyUtils(Context context) {
+        mContext = context;
+        mDolbyAtmos = new DolbyAtmos(EFFECT_PRIORITY, 0);
+        mAudioManager = context.getSystemService(AudioManager.class);
+        dlog("initalized");
+    }
+
+    public static synchronized DolbyUtils getInstance(Context context) {
+        if (mInstance == null) {
+            mInstance = new DolbyUtils(context);
+        }
+        return mInstance;
+    }
+
+    public void onBootCompleted() {
+        dlog("onBootCompleted");
+
+        // Restore current profile now and on certain audio changes.
+        final boolean dsOn = getDsOn();
+        mDolbyAtmos.setEnabled(dsOn);
+        registerCallbacks(dsOn);
+        if (dsOn)
+            setCurrentProfile();
+
+        // Restore speaker virtualizer, because for some reason it isn't
+        // enabled automatically at boot.
+        final AudioDeviceAttributes device =
+                mAudioManager.getDevicesForAttributes(ATTRIBUTES_MEDIA).get(0);
+        final boolean isOnSpeaker = (device.getType() == AudioDeviceInfo.TYPE_BUILTIN_SPEAKER);
+        final boolean spkVirtEnabled = getSpeakerVirtualizerEnabled();
+        dlog("isOnSpeaker=" + isOnSpeaker + " spkVirtEnabled=" + spkVirtEnabled);
+        if (isOnSpeaker && spkVirtEnabled) {
+            setSpeakerVirtualizerEnabled(false);
+            setSpeakerVirtualizerEnabled(true);
+            Log.i(TAG, "re-enabled speaker virtualizer");
+        }
+    }
+
+    private void checkEffect() {
+        if (!mDolbyAtmos.hasControl()) {
+            Log.w(TAG, "lost control, recreating effect");
+            mDolbyAtmos.release();
+            mDolbyAtmos = new DolbyAtmos(EFFECT_PRIORITY, 0);
+        }
+    }
+
+    private void setCurrentProfile() {
+        if (!getDsOn()) {
+            dlog("setCurrentProfile: skip, dolby is off");
+            return;
+        }
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(mContext);
+        int profile = Integer.parseInt(prefs.getString(
+                DolbySettingsFragment.PREF_PROFILE, "0" /*dynamic*/));
+        setProfile(profile);
+    }
+
+    private void registerCallbacks(boolean register) {
+        dlog("registerCallbacks(" + register + ") mCallbacksRegistered=" + mCallbacksRegistered);
+        if (register && !mCallbacksRegistered) {
+            mAudioManager.registerAudioPlaybackCallback(mPlaybackCallback, mHandler);
+            mAudioManager.registerAudioDeviceCallback(mAudioDeviceCallback, mHandler);
+            mCallbacksRegistered = true;
+        } else if (!register && mCallbacksRegistered) {
+            mAudioManager.unregisterAudioPlaybackCallback(mPlaybackCallback);
+            mAudioManager.unregisterAudioDeviceCallback(mAudioDeviceCallback);
+            mCallbacksRegistered = false;
+        }
+    }
+
+    public void setDsOn(boolean on) {
+        checkEffect();
+        dlog("setDsOn: " + on);
+        mDolbyAtmos.setDsOn(on);
+        registerCallbacks(on);
+        if (on)
+            setCurrentProfile();
+    }
+
+    public boolean getDsOn() {
+        boolean on = mDolbyAtmos.getDsOn();
+        dlog("getDsOn: " + on);
+        return on;
+    }
+
+    public void setProfile(int index) {
+        checkEffect();
+        dlog("setProfile: " + index);
+        mDolbyAtmos.setProfile(index);
+    }
+
+    public int getProfile() {
+        int profile = mDolbyAtmos.getProfile();
+        dlog("getProfile: " + profile);
+        return profile;
+    }
+
+    public String getProfileName() {
+        String profile = Integer.toString(mDolbyAtmos.getProfile());
+        List<String> profiles = Arrays.asList(mContext.getResources().getStringArray(
+                R.array.dolby_profile_values));
+        int profileIndex = profiles.indexOf(profile);
+        dlog("getProfileName: profile=" + profile + " index=" + profileIndex);
+        return profileIndex == -1 ? null : mContext.getResources().getStringArray(
+                R.array.dolby_profile_entries)[profileIndex];
+    }
+
+    public void resetProfileSpecificSettings() {
+        checkEffect();
+        mDolbyAtmos.resetProfileSpecificSettings();
+    }
+
+    public void setPreset(String preset) {
+        checkEffect();
+        int[] gains = Arrays.stream(preset.split(",")).mapToInt(Integer::parseInt).toArray();
+        dlog("setPreset: " + Arrays.toString(gains));
+        mDolbyAtmos.setDapParameter(DsParam.GEQ_BAND_GAINS, gains);
+    }
+
+    public String getPreset() {
+        int[] gains = mDolbyAtmos.getDapParameter(DsParam.GEQ_BAND_GAINS);
+        dlog("getPreset: " + Arrays.toString(gains));
+        String[] preset = Arrays.stream(gains).mapToObj(String::valueOf).toArray(String[]::new);
+        return String.join(",", preset);
+    }
+
+    public void setHeadphoneVirtualizerEnabled(boolean enable) {
+        checkEffect();
+        dlog("setHeadphoneVirtualizerEnabled: " + enable);
+        mDolbyAtmos.setDapParameterBool(DsParam.HEADPHONE_VIRTUALIZER, enable);
+    }
+
+    public boolean getHeadphoneVirtualizerEnabled() {
+        boolean enabled = mDolbyAtmos.getDapParameterBool(DsParam.HEADPHONE_VIRTUALIZER);
+        dlog("getHeadphoneVirtualizerEnabled: " + enabled);
+        return enabled;
+    }
+
+    public void setSpeakerVirtualizerEnabled(boolean enable) {
+        checkEffect();
+        dlog("setSpeakerVirtualizerEnabled: " + enable);
+        mDolbyAtmos.setDapParameterBool(DsParam.SPEAKER_VIRTUALIZER, enable);
+    }
+
+    public boolean getSpeakerVirtualizerEnabled() {
+        boolean enabled = mDolbyAtmos.getDapParameterBool(DsParam.SPEAKER_VIRTUALIZER);
+        dlog("getSpeakerVirtualizerEnabled: " + enabled);
+        return enabled;
+    }
+
+    public void setStereoWideningAmount(int amount) {
+        checkEffect();
+        dlog("setStereoWideningAmount: " + amount);
+        mDolbyAtmos.setDapParameterInt(DsParam.STEREO_WIDENING_AMOUNT, amount);
+    }
+
+    public int getStereoWideningAmount() {
+        int amount = mDolbyAtmos.getDapParameterInt(DsParam.STEREO_WIDENING_AMOUNT);
+        dlog("getStereoWideningAmount: " + amount);
+        return amount;
+    }
+
+    public void setDialogueEnhancerAmount(int amount) {
+        checkEffect();
+        dlog("setDialogueEnhancerAmount: " + amount);
+        mDolbyAtmos.setDapParameterBool(DsParam.DIALOGUE_ENHANCER_ENABLE, amount > 0);
+        mDolbyAtmos.setDapParameterInt(DsParam.DIALOGUE_ENHANCER_AMOUNT, amount);
+    }
+
+    public int getDialogueEnhancerAmount() {
+        boolean enabled = mDolbyAtmos.getDapParameterBool(
+                DsParam.DIALOGUE_ENHANCER_ENABLE);
+        int amount = enabled ? mDolbyAtmos.getDapParameterInt(
+                DsParam.DIALOGUE_ENHANCER_AMOUNT) : 0;
+        dlog("getDialogueEnhancerAmount: " + enabled + " amount=" + amount);
+        return amount;
+    }
+
+    public void setBassEnhancerEnabled(boolean enable) {
+        checkEffect();
+        dlog("setBassEnhancerEnabled: " + enable);
+        mDolbyAtmos.setDapParameterBool(DsParam.BASS_ENHANCER_ENABLE, enable);
+    }
+
+    public boolean getBassEnhancerEnabled() {
+        boolean enabled = mDolbyAtmos.getDapParameterBool(DsParam.BASS_ENHANCER_ENABLE);
+        dlog("getBassEnhancerEnabled: " + enabled);
+        return enabled;
+    }
+
+    public void setVolumeLevelerEnabled(boolean enable) {
+        checkEffect();
+        dlog("setVolumeLevelerEnabled: " + enable);
+        mDolbyAtmos.setDapParameterBool(DsParam.VOLUME_LEVELER_ENABLE, enable);
+        mDolbyAtmos.setDapParameterInt(DsParam.VOLUME_LEVELER_AMOUNT,
+                enable ? VOLUME_LEVELER_AMOUNT : 0);
+    }
+
+    public boolean getVolumeLevelerEnabled() {
+        boolean enabled = mDolbyAtmos.getDapParameterBool(DsParam.VOLUME_LEVELER_ENABLE);
+        int amount = mDolbyAtmos.getDapParameterInt(DsParam.VOLUME_LEVELER_AMOUNT);
+        dlog("getVolumeLevelerEnabled: " + enabled + " amount=" + amount);
+        return enabled && (amount == VOLUME_LEVELER_AMOUNT);
+    }
+
+    private static void dlog(String msg) {
+        if (Log.isLoggable(TAG, Log.DEBUG)) {
+            Log.d(TAG, msg);
+        }
+    }
+}

TheParasiteProject/hardware_parasite_android

Dolby/src/co/aospa/dolby/BootCompletedReceiver.java
@@ -1,0 +1,29 @@
+/*
+ * Copyright (C) 2023-24 Paranoid Android
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package co.aospa.dolby;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class BootCompletedReceiver extends BroadcastReceiver {
+
+    private static final String TAG = "Parts-BCR";
+
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        Log.d(TAG, "Received intent: " + intent.getAction());
+        if (!intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+            return;
+        }
+
+        Log.i(TAG, "Boot completed, starting dolby");
+        DolbyUtils.getInstance(context).onBootCompleted();
+    }
+
+}

Dolby/src/co/aospa/dolby/DolbyUtils.java
@@ -1,0 +1,281 @@
+/*
+ * Copyright (C) 2023-24 Paranoid Android
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package co.aospa.dolby;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.media.AudioAttributes;
+import android.media.AudioDeviceAttributes;
+import android.media.AudioDeviceCallback;
+import android.media.AudioDeviceInfo;
+import android.media.AudioManager;
+import android.media.AudioManager.AudioPlaybackCallback;
+import android.media.AudioPlaybackConfiguration;
+import android.media.session.MediaSessionManager;
+import android.os.Handler;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import co.aospa.dolby.DolbyConstants.DsParam;
+import co.aospa.dolby.R;
+
+import java.util.Arrays;
+import java.util.List;
+
+public final class DolbyUtils {
+
+    private static final String TAG = "DolbyUtils";
+    private static final int EFFECT_PRIORITY = 100;
+    private static final int VOLUME_LEVELER_AMOUNT = 2;
+
+    private static final AudioAttributes ATTRIBUTES_MEDIA = new AudioAttributes.Builder()
+            .setUsage(AudioAttributes.USAGE_MEDIA)
+            .build();
+
+    private static DolbyUtils mInstance;
+    private DolbyAtmos mDolbyAtmos;
+    private Context mContext;
+    private AudioManager mAudioManager;
+    private Handler mHandler = new Handler();
+    private boolean mCallbacksRegistered = false;
+
+    // Restore current profile on every media session
+    private final AudioPlaybackCallback mPlaybackCallback = new AudioPlaybackCallback() {
+        @Override
+        public void onPlaybackConfigChanged(List<AudioPlaybackConfiguration> configs) {
+            boolean isPlaying = configs.stream().anyMatch(
+                    c -> c.getPlayerState() == AudioPlaybackConfiguration.PLAYER_STATE_STARTED);
+            dlog("onPlaybackConfigChanged isPlaying=" + isPlaying);
+            if (isPlaying)
+                setCurrentProfile();
+        }
+    };
+
+    // Restore current profile on audio device change
+    private final AudioDeviceCallback mAudioDeviceCallback = new AudioDeviceCallback() {
+        public void onAudioDevicesAdded(AudioDeviceInfo[] addedDevices) {
+            dlog("onAudioDevicesAdded");
+            setCurrentProfile();
+        }
+
+        public void onAudioDevicesRemoved(AudioDeviceInfo[] removedDevices) {
+            dlog("onAudioDevicesRemoved");
+            setCurrentProfile();
+        }
+    };
+
+    private DolbyUtils(Context context) {
+        mContext = context;
+        mDolbyAtmos = new DolbyAtmos(EFFECT_PRIORITY, 0);
+        mAudioManager = context.getSystemService(AudioManager.class);
+        dlog("initalized");
+    }
+
+    public static synchronized DolbyUtils getInstance(Context context) {
+        if (mInstance == null) {
+            mInstance = new DolbyUtils(context);
+        }
+        return mInstance;
+    }
+
+    public void onBootCompleted() {
+        dlog("onBootCompleted");
+
+        // Restore current profile now and on certain audio changes.
+        final boolean dsOn = getDsOn();
+        mDolbyAtmos.setEnabled(dsOn);
+        registerCallbacks(dsOn);
+        if (dsOn)
+            setCurrentProfile();
+
+        // Restore speaker virtualizer, because for some reason it isn't
+        // enabled automatically at boot.
+        final AudioDeviceAttributes device =
+                mAudioManager.getDevicesForAttributes(ATTRIBUTES_MEDIA).get(0);
+        final boolean isOnSpeaker = (device.getType() == AudioDeviceInfo.TYPE_BUILTIN_SPEAKER);
+        final boolean spkVirtEnabled = getSpeakerVirtualizerEnabled();
+        dlog("isOnSpeaker=" + isOnSpeaker + " spkVirtEnabled=" + spkVirtEnabled);
+        if (isOnSpeaker && spkVirtEnabled) {
+            setSpeakerVirtualizerEnabled(false);
+            setSpeakerVirtualizerEnabled(true);
+            Log.i(TAG, "re-enabled speaker virtualizer");
+        }
+    }
+
+    private void checkEffect() {
+        if (!mDolbyAtmos.hasControl()) {
+            Log.w(TAG, "lost control, recreating effect");
+            mDolbyAtmos.release();
+            mDolbyAtmos = new DolbyAtmos(EFFECT_PRIORITY, 0);
+        }
+    }
+
+    private void setCurrentProfile() {
+        if (!getDsOn()) {
+            dlog("setCurrentProfile: skip, dolby is off");
+            return;
+        }
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(mContext);
+        int profile = Integer.parseInt(prefs.getString(
+                DolbySettingsFragment.PREF_PROFILE, "0" /*dynamic*/));
+        setProfile(profile);
+    }
+
+    private void registerCallbacks(boolean register) {
+        dlog("registerCallbacks(" + register + ") mCallbacksRegistered=" + mCallbacksRegistered);
+        if (register && !mCallbacksRegistered) {
+            mAudioManager.registerAudioPlaybackCallback(mPlaybackCallback, mHandler);
+            mAudioManager.registerAudioDeviceCallback(mAudioDeviceCallback, mHandler);
+            mCallbacksRegistered = true;
+        } else if (!register && mCallbacksRegistered) {
+            mAudioManager.unregisterAudioPlaybackCallback(mPlaybackCallback);
+            mAudioManager.unregisterAudioDeviceCallback(mAudioDeviceCallback);
+            mCallbacksRegistered = false;
+        }
+    }
+
+    public void setDsOn(boolean on) {
+        checkEffect();
+        dlog("setDsOn: " + on);
+        mDolbyAtmos.setDsOn(on);
+        registerCallbacks(on);
+        if (on)
+            setCurrentProfile();
+    }
+
+    public boolean getDsOn() {
+        boolean on = mDolbyAtmos.getDsOn();
+        dlog("getDsOn: " + on);
+        return on;
+    }
+
+    public void setProfile(int index) {
+        checkEffect();
+        dlog("setProfile: " + index);
+        mDolbyAtmos.setProfile(index);
+    }
+
+    public int getProfile() {
+        int profile = mDolbyAtmos.getProfile();
+        dlog("getProfile: " + profile);
+        return profile;
+    }
+
+    public String getProfileName() {
+        String profile = Integer.toString(mDolbyAtmos.getProfile());
+        List<String> profiles = Arrays.asList(mContext.getResources().getStringArray(
+                R.array.dolby_profile_values));
+        int profileIndex = profiles.indexOf(profile);
+        dlog("getProfileName: profile=" + profile + " index=" + profileIndex);
+        return profileIndex == -1 ? null : mContext.getResources().getStringArray(
+                R.array.dolby_profile_entries)[profileIndex];
+    }
+
+    public void resetProfileSpecificSettings() {
+        checkEffect();
+        mDolbyAtmos.resetProfileSpecificSettings();
+    }
+
+    public void setPreset(String preset) {
+        checkEffect();
+        int[] gains = Arrays.stream(preset.split(",")).mapToInt(Integer::parseInt).toArray();
+        dlog("setPreset: " + Arrays.toString(gains));
+        mDolbyAtmos.setDapParameter(DsParam.GEQ_BAND_GAINS, gains);
+    }
+
+    public String getPreset() {
+        int[] gains = mDolbyAtmos.getDapParameter(DsParam.GEQ_BAND_GAINS);
+        dlog("getPreset: " + Arrays.toString(gains));
+        String[] preset = Arrays.stream(gains).mapToObj(String::valueOf).toArray(String[]::new);
+        return String.join(",", preset);
+    }
+
+    public void setHeadphoneVirtualizerEnabled(boolean enable) {
+        checkEffect();
+        dlog("setHeadphoneVirtualizerEnabled: " + enable);
+        mDolbyAtmos.setDapParameterBool(DsParam.HEADPHONE_VIRTUALIZER, enable);
+    }
+
+    public boolean getHeadphoneVirtualizerEnabled() {
+        boolean enabled = mDolbyAtmos.getDapParameterBool(DsParam.HEADPHONE_VIRTUALIZER);
+        dlog("getHeadphoneVirtualizerEnabled: " + enabled);
+        return enabled;
+    }
+
+    public void setSpeakerVirtualizerEnabled(boolean enable) {
+        checkEffect();
+        dlog("setSpeakerVirtualizerEnabled: " + enable);
+        mDolbyAtmos.setDapParameterBool(DsParam.SPEAKER_VIRTUALIZER, enable);
+    }
+
+    public boolean getSpeakerVirtualizerEnabled() {
+        boolean enabled = mDolbyAtmos.getDapParameterBool(DsParam.SPEAKER_VIRTUALIZER);
+        dlog("getSpeakerVirtualizerEnabled: " + enabled);
+        return enabled;
+    }
+
+    public void setStereoWideningAmount(int amount) {
+        checkEffect();
+        dlog("setStereoWideningAmount: " + amount);
+        mDolbyAtmos.setDapParameterInt(DsParam.STEREO_WIDENING_AMOUNT, amount);
+    }
+
+    public int getStereoWideningAmount() {
+        int amount = mDolbyAtmos.getDapParameterInt(DsParam.STEREO_WIDENING_AMOUNT);
+        dlog("getStereoWideningAmount: " + amount);
+        return amount;
+    }
+
+    public void setDialogueEnhancerAmount(int amount) {
+        checkEffect();
+        dlog("setDialogueEnhancerAmount: " + amount);
+        mDolbyAtmos.setDapParameterBool(DsParam.DIALOGUE_ENHANCER_ENABLE, amount > 0);
+        mDolbyAtmos.setDapParameterInt(DsParam.DIALOGUE_ENHANCER_AMOUNT, amount);
+    }
+
+    public int getDialogueEnhancerAmount() {
+        boolean enabled = mDolbyAtmos.getDapParameterBool(
+                DsParam.DIALOGUE_ENHANCER_ENABLE);
+        int amount = enabled ? mDolbyAtmos.getDapParameterInt(
+                DsParam.DIALOGUE_ENHANCER_AMOUNT) : 0;
+        dlog("getDialogueEnhancerAmount: " + enabled + " amount=" + amount);
+        return amount;
+    }
+
+    public void setBassEnhancerEnabled(boolean enable) {
+        checkEffect();
+        dlog("setBassEnhancerEnabled: " + enable);
+        mDolbyAtmos.setDapParameterBool(DsParam.BASS_ENHANCER_ENABLE, enable);
+    }
+
+    public boolean getBassEnhancerEnabled() {
+        boolean enabled = mDolbyAtmos.getDapParameterBool(DsParam.BASS_ENHANCER_ENABLE);
+        dlog("getBassEnhancerEnabled: " + enabled);
+        return enabled;
+    }
+
+    public void setVolumeLevelerEnabled(boolean enable) {
+        checkEffect();
+        dlog("setVolumeLevelerEnabled: " + enable);
+        mDolbyAtmos.setDapParameterBool(DsParam.VOLUME_LEVELER_ENABLE, enable);
+        mDolbyAtmos.setDapParameterInt(DsParam.VOLUME_LEVELER_AMOUNT,
+                enable ? VOLUME_LEVELER_AMOUNT : 0);
+    }
+
+    public boolean getVolumeLevelerEnabled() {
+        boolean enabled = mDolbyAtmos.getDapParameterBool(DsParam.VOLUME_LEVELER_ENABLE);
+        int amount = mDolbyAtmos.getDapParameterInt(DsParam.VOLUME_LEVELER_AMOUNT);
+        dlog("getVolumeLevelerEnabled: " + enabled + " amount=" + amount);
+        return enabled && (amount == VOLUME_LEVELER_AMOUNT);
+    }
+
+    private static void dlog(String msg) {
+        if (Log.isLoggable(TAG, Log.DEBUG)) {
+            Log.d(TAG, msg);
+        }
+    }
+}